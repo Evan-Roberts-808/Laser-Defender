1925e5a09fa73b9d18ca8dbfa1bbdd46347d24e8
Evan Roberts <eroberts1998@icloud.com>
2024-01-30 07:01:20 -0500

    restrict player movement to specific boundaries based on camera size

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,24 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     Vector2 rawInput;
+    Vector2 minBound;
+    Vector2 maxBound;
 
     [SerializeField] float moveSpeed = 15f;
+    [SerializeField] float paddingLeft = 0.5f;
+    [SerializeField] float paddingRight = 0.5f;
+    [SerializeField] float paddingTop = 10f;
+    [SerializeField] float paddingBottom = 2f;
+
+    private void Start() {
+        InitBounds();
+    }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 delta = Move();
-        transform.position += delta;
+        Move();
     }
 
-    private Vector3 Move()
+    private void InitBounds()
     {
-        return rawInput * moveSpeed * Time.deltaTime;
+        Camera mainCamera = Camera.main;
+        minBound = mainCamera.ViewportToWorldPoint(new Vector2(0,0));
+        maxBound = mainCamera.ViewportToWorldPoint(new Vector2(1,1));
+    }
+
+    void Move()
+    {
+        Vector2 delta = rawInput * moveSpeed * Time.deltaTime;
+        Vector2 newPos = new Vector2();
+        newPos.x = Mathf.Clamp(transform.position.x + delta.x, minBound.x + paddingLeft, maxBound.x - paddingRight);
+        newPos.y = Mathf.Clamp(transform.position.y + delta.y, minBound.y + paddingBottom, maxBound.y - paddingTop);
+        transform.position = newPos;
     }
 
     private void OnMove(InputValue value)
