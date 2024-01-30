aa922c1ba393cc2194cee6fc0aa3dbbe3c03d548
Evan Roberts <eroberts1998@icloud.com>
2024-01-30 07:36:06 -0500

    create scriptable object for enemy pathfinding

---
+++ Assets/Scripts/Pathfinder.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinder : MonoBehaviour
+{
+    [SerializeField] WaveConfigSO waveConfig;
+    List<Transform> waypoints;
+    int waypointIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        waypoints = waveConfig.GetWaypoints();
+        transform.position = waypoints[waypointIndex].position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowPath();
+    }
+
+    void FollowPath()
+    {
+        if (waypointIndex < waypoints.Count)
+        {
+            Vector3 targetPosition = waypoints[waypointIndex].position;
+            float delta = waveConfig.GetMoveSpeed() * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, delta);
+            if (transform.position == targetPosition)
+            {
+                waypointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
