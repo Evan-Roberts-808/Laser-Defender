73a4f4c05b85e61598ba0452b4863fb05e8117c7
Evan Roberts <eroberts1998@icloud.com>
2024-02-01 18:28:55 -0500

    add damage on collision and basic health system

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] int health = 50;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        DamageDealer damageDealer = other.GetComponent<DamageDealer>();
+        if (damageDealer != null) {
+            TakeDamage(damageDealer.GetDamage());
+            damageDealer.Hit();
+            
+        }
+    }
+
+    private void TakeDamage(int damage){
+        health -= damage;
+        if (health == 0) {
+            Destroy(gameObject);
+        }
+    }
+}
