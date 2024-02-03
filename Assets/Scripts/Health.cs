0ee18289f42a23f6039f737e723fe8a7f25fb79d
Evan Roberts <eroberts1998@icloud.com>
2024-02-03 09:48:37 -0500

    add particle system on hit to add an explosion effect to player and enemies when hit by a shot

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class Health : MonoBehaviour
 {
     [SerializeField] int health = 50;
+    [SerializeField] ParticleSystem explosionEffect;
 
     private void OnTriggerEnter2D(Collider2D other) {
         DamageDealer damageDealer = other.GetComponent<DamageDealer>();
         if (damageDealer != null) {
             TakeDamage(damageDealer.GetDamage());
+            PlayExplosionEffect();
             damageDealer.Hit();
             
         }
@@ -21,4 +23,12 @@ public class Health : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
+    void PlayExplosionEffect(){
+        if (explosionEffect != null) {
+            ParticleSystem instance = Instantiate(explosionEffect, transform.position, Quaternion.identity);
+            Destroy(instance.gameObject, instance.main.duration + instance.main.startLifetime.constantMax);
+        }
+    }
+
 }
