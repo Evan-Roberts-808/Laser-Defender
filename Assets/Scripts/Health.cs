f56cf54c244e1eccbd50b7cb94c2566be704b39a
Evan Roberts <eroberts1998@icloud.com>
2024-02-03 09:59:29 -0500

    add camera shake when the player takes damage

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -6,12 +6,20 @@ public class Health : MonoBehaviour
 {
     [SerializeField] int health = 50;
     [SerializeField] ParticleSystem explosionEffect;
+    [SerializeField] bool applyCameraShake;
+
+    CameraShake cameraShake;
+
+    private void Awake() {
+        cameraShake = Camera.main.GetComponent<CameraShake>();
+    }
 
     private void OnTriggerEnter2D(Collider2D other) {
         DamageDealer damageDealer = other.GetComponent<DamageDealer>();
         if (damageDealer != null) {
             TakeDamage(damageDealer.GetDamage());
             PlayExplosionEffect();
+            ShakeCamera();
             damageDealer.Hit();
             
         }
@@ -31,4 +39,9 @@ public class Health : MonoBehaviour
         }
     }
 
+    void ShakeCamera(){
+        if (cameraShake != null && applyCameraShake) {
+            cameraShake.Play();
+        }
+    }
 }
